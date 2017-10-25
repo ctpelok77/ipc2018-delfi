0ad1cffd58a1486cb688bb326f547b7c48574faf
Silvan Sievers <silvan.sievers@unibas.ch>
2017-10-25 13:50:06 +0200

    re-run exp

--- experiments/symmetry-reduction/2017-10-25-2transpositions.py
+++ experiments/symmetry-reduction/2017-10-25-2transpositions.py
@@ -14,7 +14,7 @@ except ImportError:
     print 'matplotlib not availabe, scatter plots not available'
     matplotlib = False
 
-REVISION = '402965425b0d'
+REVISION = '8d424d25a62d'
 
 def main(revisions=None):
     benchmarks_dir=os.path.expanduser('~/repos/downward/benchmarks')
