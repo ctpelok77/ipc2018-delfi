1655e8f74829e47f3a949328de34419bb2c4e7ed
Silvan Sievers <silvan.sievers@unibas.ch>
2018-01-15 19:18:02 +0100

    add more configs

--- experiments/ipc2018/2018-01-15-h2-oldsemantics-oss-configs.py
+++ experiments/ipc2018/2018-01-15-h2-oldsemantics-oss-configs.py
@@ -37,6 +37,8 @@ def main(revisions=None):
     'transport-opt14-strips', 'trucks', 'trucks-strips',
     'visitall-opt11-strips', 'visitall-opt14-strips',
     'woodworking-opt08-strips', 'woodworking-opt11-strips', 'zenotravel']
+    # TODO: possiblly incomplete subset of non-axiomatic domains with conditional effects:
+    # ​[​'cavediving-14-adl', 'citycar-opt14-adl', 'maintenance-opt14-adl', 'miconic-simpleadl', 'openstacks-opt08-adl', 'pathways', 'schedule', 'trucks'​]​
 
     environment = BaselSlurmEnvironment(email="silvan.sievers@unibas.ch", export=["PATH"])
 
@@ -45,11 +47,15 @@ def main(revisions=None):
         environment = LocalEnvironment(processes=4)
 
     configs = {
-        IssueConfig('h2-oldsemantics-oss-blind', ['--translate-options', '--enforce-definite-effects', '--search-options', '--symmetries', 'sym=structural_symmetries(search_symmetries=oss)', '--search', 'astar(blind,symmetries=sym)'], driver_options=['--transform', 'h2-mutexes']),
-        IssueConfig('h2-oldsemantics-oss-celmcut', ['--translate-options', '--enforce-definite-effects', '--search-options', '--symmetries', 'sym=structural_symmetries(search_symmetries=oss)', '--search', 'astar(celmcut,symmetries=sym)'], driver_options=['--transform', 'h2-mutexes']),
-        # IssueConfig('h2-oldsemantics-oss-lmcount', ['--translate-options', '--enforce-definite-effects', '--search-options', '--symmetries', 'sym=structural_symmetries(search_symmetries=oss)', '--search', 'astar(lmcount(lm_factory=,admissible=true),symmetries=sym)'], driver_options=['--transform', 'h2-mutexes']),
-        # TODO: M&S
+        IssueConfig('h2-simpless-oldsemantics-oss-blind', ['--translate-options', '--enforce-definite-effects', '--search-options', '--symmetries', 'sym=structural_symmetries(search_symmetries=oss)', '--search', 'astar(blind,symmetries=sym,pruning=stubborn_sets_simple(minimum_pruning_ratio=0.01), num_por_probes=1000)'], driver_options=['--transform', 'h2-mutexes']),
+        IssueConfig('h2-simpless-oldsemantics-oss-celmcut', ['--translate-options', '--enforce-definite-effects', '--search-options', '--symmetries', 'sym=structural_symmetries(search_symmetries=oss)', '--search', 'astar(celmcut,symmetries=sym,pruning=stubborn_sets_simple(minimum_pruning_ratio=0.01), num_por_probes=1000)'], driver_options=['--transform', 'h2-mutexes']),
+        IssueConfig('h2-simpless-oldsemantics-oss-lmcountlmrhw', ['--translate-options', '--enforce-definite-effects', '--search-options', '--symmetries', 'sym=structural_symmetries(search_symmetries=oss)', '--search', 'astar(lmcount(lm_factory=lm_rhw,admissible=true),symmetries=sym,pruning=stubborn_sets_simple(minimum_pruning_ratio=0.01), num_por_probes=1000,mpd=true)'], driver_options=['--transform', 'h2-mutexes']),
+        IssueConfig('h2-simpless-oldsemantics-oss-lmcountlmmergedlmrhwlmhm1', ['--translate-options', '--enforce-definite-effects', '--search-options', '--symmetries', 'sym=structural_symmetries(search_symmetries=oss)', '--search', 'astar(lmcount(lm_factory=lm_merged([lm_rhw,lm_hm(m=1)]),admissible=true),symmetries=sym,pruning=stubborn_sets_simple(minimum_pruning_ratio=0.01), num_por_probes=1000,mpd=true)'], driver_options=['--transform', 'h2-mutexes']),
+        IssueConfig('h2-simpless-oldsemantics-oss-masb50ksccdfp', ['--translate-options', '--enforce-definite-effects', '--search-options', '--symmetries', 'sym=structural_symmetries(search_symmetries=oss)', '--search', 'astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false),merge_strategy=merge_sccs(order_of_sccs=topological,merge_selector=score_based_filtering(scoring_functions=[goal_relevance,dfp,total_order(atomic_before_product=false,atomic_ts_order=reverse_level,product_ts_order=new_to_old)])),label_reduction=exact(before_shrinking=true,before_merging=false),max_states=50000,threshold_before_merge=1),symmetries=sym,pruning=stubborn_sets_simple(minimum_pruning_ratio=0.01), num_por_probes=1000,mpd=true)'], driver_options=['--transform', 'h2-mutexes']),
+        IssueConfig('h2-simpless-oldsemantics-oss-masb50ksbmiasm', ['--translate-options', '--enforce-definite-effects', '--search-options', '--symmetries', 'sym=structural_symmetries(search_symmetries=oss)', '--search', 'astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false),merge_strategy=merge_stateless(merge_selector=score_based_filtering(scoring_functions=[sf_miasm(shrink_strategy=shrink_bisimulation,max_states=50000),total_order(atomic_before_product=true,atomic_ts_order=reverse_level,product_ts_order=old_to_new)])),label_reduction=exact(before_shrinking=true,before_merging=false),max_states=50000,threshold_before_merge=1),symmetries=sym,pruning=stubborn_sets_simple(minimum_pruning_ratio=0.01), num_por_probes=1000,mpd=true)'], driver_options=['--transform', 'h2-mutexes']), # TODO: use best tie-breaking
+        # TODO: integrate M&S MIASM?
         # TODO: iPDB/PDB: no support for conditional effects
+        # TODO: check with Florian/Jendrik about potentials/LP-heuristics, and think about whether we really want to go through the CPLEX hassle
     }
 
     exp = IssueExperiment(
