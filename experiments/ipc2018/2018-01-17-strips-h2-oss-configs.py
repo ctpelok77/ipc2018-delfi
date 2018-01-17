bd1e371f87c7a598eaa97d9e670307543a888028
Silvan Sievers <silvan.sievers@unibas.ch>
2018-01-17 15:08:20 +0100

    use rev

--- experiments/ipc2018/2018-01-17-strips-h2-oss-configs.py
+++ experiments/ipc2018/2018-01-17-strips-h2-oss-configs.py
@@ -7,7 +7,7 @@ from lab.environments import LocalEnvironment, BaselSlurmEnvironment
 
 from common_setup import IssueConfig, IssueExperiment, is_test_run
 
-REVISION = '24a4e5560f24'
+REVISION = 'eab7725155f4'
 
 def main(revisions=None):
     benchmarks_dir = os.environ["DOWNWARD_BENCHMARKS_IPC2018"]
