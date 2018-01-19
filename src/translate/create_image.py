b4b449433d8627f0c8fe760ba2b316e4c0b503f2
Michael Katz <ctpelok@gmail.com>
2018-01-19 15:44:34 -0500

    integrating the part that creates abstract structure graph and image for it, excluding pybliss and automorphisms

---
+++ src/translate/create_image.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+
+import normalize
+import options
+import pddl_parser
+from abstract_structure_module import AbstractStructureGraph
+import sys
+import timers
+
+
+if __name__ == "__main__":
+    timer = timers.Timer()
+
+    only_object_symmetries = options.only_object_symmetries
+    stabilize_initial_state = not options.do_not_stabilize_initial_state
+    stabilize_goal = not options.do_not_stabilize_goal
+
+    use_bolding = options.bolding_abstract_structure_image 
+    write_original_size = options.write_abstract_structure_image_original_size
+    abstract_structure_image_target_size = options.abstract_structure_image_target_size
+
+    with timers.timing("Parsing pddl..", True):    
+        task = pddl_parser.open()
+    with timers.timing("Normalizing task..", True):    
+        normalize.normalize(task)
+    #print("Dumping task..")
+    #task.dump()
+    with timers.timing("Creating abstract structure graph..", True):    
+        graph = AbstractStructureGraph(task, only_object_symmetries, stabilize_initial_state, stabilize_goal)
+    if options.dump_dot_graph:
+        f = open('out.dot', 'w')
+        graph.write_dot_graph(f, hide_equal_predicates=True)
+        f.close()
+    if options.write_abstract_structure_image_raw:
+        with timers.timing("Writing abstract structure graph raw image..", True):    
+            graph.write_matrix_image_grayscale(hide_equal_predicates=True, shrink_ratio=1, bolded=use_bolding, target_size=abstract_structure_image_target_size, write_original_size=write_original_size)
+    if options.write_abstract_structure_image_reg:
+        with timers.timing("Writing abstract structure graph grayscale 8bit image..", True):    
+            graph.write_matrix_image_grayscale(hide_equal_predicates=True, shrink_ratio=3, bolded=use_bolding, target_size=abstract_structure_image_target_size, write_original_size=write_original_size)
+    if options.write_abstract_structure_image_int:
+        with timers.timing("Writing abstract structure graph grayscale 32bit image..", True):    
+            graph.write_matrix_image_grayscale(hide_equal_predicates=True, shrink_ratio=6, bolded=use_bolding, target_size=abstract_structure_image_target_size, write_original_size=write_original_size)
+        
+    print("Done! %s" % timer)
+
+    sys.stdout.flush()
+
