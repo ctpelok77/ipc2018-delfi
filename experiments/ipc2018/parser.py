e09cdba3aafa2beb548c453744ec54df603df9b2
Silvan Sievers <silvan.sievers@unibas.ch>
2018-01-22 23:23:33 +0100

    add experiments to find some good genetic pdb configs; add greedy bisim mas config

---
+++ experiments/ipc2018/parser.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from lab.parser import Parser
+
+parser = Parser()
+
+parser.add_pattern('cpdbs_num_pdbs', 'CanonicalPDBs: number of pdbs = (\d+)', required=False, type=int)
+parser.add_pattern('cpdbs_num_pdb_lookups', 'CanonicalPDBs: number of required pdb lookups = (\d+)', required=False, type=int)
+parser.add_pattern('cpdbs_estimated_memory_usage', 'CanonicalPDBs: estimated memory usage = (\d+)', required=False, type=int)
+parser.add_pattern('cpdbs_size', 'CanonicalPDBs: size = (\d+)', required=False, type=int)
+parser.add_pattern('genetic_time', 'Pattern generation \(Edelkamp\) time: (.+)s', required=False, type=float)
+
+parser.parse()
