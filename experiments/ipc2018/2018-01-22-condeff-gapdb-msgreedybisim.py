4b50b7c69db1c3ff3865e921397e8fa319ac598f
Silvan Sievers <silvan.sievers@unibas.ch>
2018-01-23 11:21:49 +0100

    used wrong suite

--- experiments/ipc2018/2018-01-22-condeff-gapdb-msgreedybisim.py
+++ experiments/ipc2018/2018-01-22-condeff-gapdb-msgreedybisim.py
@@ -12,28 +12,15 @@ REVISION = '40b648bf6d40'
 def main(revisions=None):
     benchmarks_dir = os.environ["DOWNWARD_BENCHMARKS_IPC2018"]
 
-    # optimal strips suite
-    suite = ['airport', 'barman-opt11-strips', 'barman-opt14-strips', 'blocks',
-    'childsnack-opt14-strips', 'depot', 'driverlog', 'elevators-opt08-strips',
-    'elevators-opt11-strips', 'floortile-opt11-strips',
-    'floortile-opt14-strips', 'freecell', 'ged-opt14-strips', 'grid',
-    'gripper', 'hiking-opt14-strips', 'logistics00', 'logistics98', 'miconic',
-    'movie', 'mprime', 'mystery', 'nomystery-opt11-strips',
-    'openstacks-opt08-strips', 'openstacks-opt11-strips',
-    'openstacks-opt14-strips', 'openstacks-strips', 'parcprinter-08-strips',
-    'parcprinter-opt11-strips', 'parking-opt11-strips', 'parking-opt14-strips',
-    'pathways-noneg', 'pegsol-08-strips', 'pegsol-opt11-strips',
-    'pipesworld-notankage', 'pipesworld-tankage', 'psr-small', 'rovers',
-    'satellite', 'scanalyzer-08-strips', 'scanalyzer-opt11-strips',
-    'sokoban-opt08-strips', 'sokoban-opt11-strips', 'storage',
-    'tetris-opt14-strips', 'tidybot-opt11-strips', 'tidybot-opt14-strips',
-    'tpp', 'transport-opt08-strips', 'transport-opt11-strips',
-    'transport-opt14-strips', 'trucks-strips', 'visitall-opt11-strips',
-    'visitall-opt14-strips', 'woodworking-opt08-strips',
-    'woodworking-opt11-strips', 'zenotravel']
+    # conditional effects suite
+    suite = ['briefcaseworld', 'cavediving-14-adl', 'citycar-opt14-adl',
+    'fsc-blocks', 'fsc-grid-a1', 'fsc-grid-a2', 'fsc-grid-r', 'fsc-hall',
+    'fsc-visualmarker', 'gedp-ds2ndp', 'miconic-simpleadl', 't0-adder',
+    't0-coins', 't0-comm', 't0-grid-dispose', 't0-grid-push', 't0-grid-trash',
+    't0-sortnet', 't0-sortnet-alt', 't0-uts']
 
     if is_test_run():
-        suite = ['gripper:prob01.pddl','depot:p01.pddl']
+        suite = ['miconic-simpleadl:s1-0.pddl']
         environment = LocalEnvironment(processes=4)
     else:
         environment = BaselSlurmEnvironment(email="silvan.sievers@unibas.ch", export=["PATH"])
