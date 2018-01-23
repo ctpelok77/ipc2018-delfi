3eee38d866b41cf26861f528786a054357235035
Silvan Sievers <silvan.sievers@unibas.ch>
2018-01-23 18:54:10 +0100

    fix comparison

--- experiments/ipc2018/2018-01-23-compare-max-vs-components.py
+++ experiments/ipc2018/2018-01-23-compare-max-vs-components.py
@@ -26,9 +26,14 @@ def main(revisions=[]):
 
     def rename(props):
         props['algorithm'] = props['algorithm'].replace('oldsemantics-', '')
+        props['algorithm'] = props['algorithm'].replace('newsemantics-', '')
         return props
 
     exp.add_fetcher('data/2018-01-18-strips-dks-configs-eval',filter_algorithm=[
+        '{}-h2-simpless-dks-lmcountlmmergedlmrhwlmhm1'.format(REVISION),
+        '{}-h2-simpless-dks-celmcut'.format(REVISION),
+    ],filter=[rename])
+    exp.add_fetcher('data/2018-01-18-condeffects-newsemantics-dks-configs-eval',filter_algorithm=[
         '{}-h2-simpless-dks-lmcountlmmergedlmrhwlmhm1'.format(REVISION),
         '{}-h2-simpless-dks-celmcut'.format(REVISION),
     ],filter=[rename])
