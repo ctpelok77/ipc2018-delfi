f123c8e9ab4425b14affe924e2c0d6d2e6109881
Silvan Sievers <silvan.sievers@unibas.ch>
2018-01-24 18:51:40 +0100

    symba run script: add time printing

--- symba.py
+++ symba.py
@@ -4,22 +4,22 @@
 import argparse
 from subprocess import call
 import sys
+sys.path.insert(1, 'driver')
+import timers
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    "--symba", action="store_true",
-    help="run symba on the given domain and plan file, ignore all other arguments")
+parser.add_argument("planner")
 parser.add_argument("domain_file")
 parser.add_argument("problem_file")
 parser.add_argument("plan_file")
 
 args = parser.parse_args()
+planner = args.planner
 domain = args.domain_file
 problem = args.problem_file
 plan_file = args.plan_file
 
-if args.symba:
-    call(['symba/plan', domain, problem, plan_file])
-else:
-    call([sys.executable, 'fast-downward.py', "--plan-file", plan_file, "--alias", "seq-opt-lmcut", domain, problem])
+timer = timers.Timer()
+call(['symba/src/plan-ipc', planner, domain, problem, plan_file])
+print("Overall time: {}".format(timer))
