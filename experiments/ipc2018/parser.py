30f2a04ee968c4f0d74f289a6e43e9c3f7c16f3a
Silvan Sievers <silvan.sievers@unibas.ch>
2018-01-24 18:48:27 +0100

    rename file

--- experiments/ipc2018/parser.py
+++ experiments/ipc2018/parser.py
@@ -9,5 +9,7 @@ parser.add_pattern('cpdbs_num_pdb_lookups', 'CanonicalPDBs: number of required p
 parser.add_pattern('cpdbs_estimated_memory_usage', 'CanonicalPDBs: estimated memory usage = (\d+)', required=False, type=int)
 parser.add_pattern('cpdbs_size', 'CanonicalPDBs: size = (\d+)', required=False, type=int)
 parser.add_pattern('genetic_time', 'Pattern generation \(Edelkamp\) time: (.+)s', required=False, type=float)
+parser.add_pattern('overall_cpu_time', 'Overall time: \[(.+)s CPU, .+s wall-clock\]', required=False, type=float)
+parser.add_pattern('overall_wallclock_time', 'Overall time: \[.+s CPU, (.+)s wall-clock\]', required=False, type=float)
 
 parser.parse()
