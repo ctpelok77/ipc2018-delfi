a6670b51bb8b5ac7adc522a71225eafcc0894ad0
Silvan Sievers <silvan.sievers@unibas.ch>
2018-01-24 22:36:58 +0100

    improve script to call symba

--- symba.py
+++ symba.py
@@ -3,22 +3,51 @@
 
 import argparse
 from subprocess import call
+import os
 import sys
 import timers
 
+def get_script():
+    """Get file name of main script."""
+    return os.path.abspath(sys.argv[0])
+
+
+def get_script_dir():
+    """Get directory of main script.
+
+    Usually a relative directory (depends on how it was called by the user.)"""
+    return os.path.dirname(get_script())
+
+def get_repo_base():
+    """Get base directory of the repository, as an absolute path.
+
+    Search upwards in the directory tree from the main script until a
+    directory with a subdirectory named ".hg" is found.
+
+    Abort if the repo base cannot be found."""
+    path = os.path.abspath(get_script_dir())
+    while os.path.dirname(path) != path:
+        if os.path.exists(os.path.join(path, ".hg")):
+            return path
+        path = os.path.dirname(path)
+    sys.exit("repo base could not be found")
+
 parser = argparse.ArgumentParser()
 
-parser.add_argument("planner")
+parser.add_argument("config")
 parser.add_argument("domain_file")
 parser.add_argument("problem_file")
 parser.add_argument("plan_file")
 
 args = parser.parse_args()
-planner = args.planner
+config = args.config
 domain = args.domain_file
 problem = args.problem_file
 plan_file = args.plan_file
 
+repo_dir = get_repo_base()
+planner = os.path.join(os.path.abspath(repo_dir), 'symba', 'src', 'plan-ipc')
+
 timer = timers.Timer()
-call(['symba/src/plan-ipc', planner, domain, problem, plan_file])
+call([planner, config, domain, problem, plan_file])
 print("Overall time: {}".format(timer))
