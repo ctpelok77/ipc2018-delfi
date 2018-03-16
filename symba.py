4d79916ce62cfab866ed8afcac952498661eec54
Silvan Sievers <silvan.sievers@unibas.ch>
2018-03-16 09:46:52 +0100

    do not rely on the existence of an .hg directory

--- symba.py
+++ symba.py
@@ -18,23 +18,13 @@ def get_script_dir():
     Usually a relative directory (depends on how it was called by the user.)"""
     return os.path.dirname(get_script())
 
-def get_repo_base():
-    """Get base directory of the repository, as an absolute path.
-
-    Search upwards in the directory tree from the main script until a
-    directory with a subdirectory named ".hg" is found.
-
-    Abort if the repo base cannot be found."""
-    path = os.path.abspath(get_script_dir())
-    while os.path.dirname(path) != path:
-        if os.path.exists(os.path.join(path, ".hg")):
-            return path
-        path = os.path.dirname(path)
-    sys.exit("repo base could not be found")
+def get_base_dir():
+    """Assume that this script always lives in the base dir of the infrastructure."""
+    return os.path.abspath(get_script_dir())
 
 def run_symba(config, domain, problem, plan):
-    repo_dir = get_repo_base()
-    planner = os.path.join(os.path.abspath(repo_dir), 'symba', 'src', 'plan-ipc')
+    base_dir = get_base_dir()
+    planner = os.path.join(os.path.abspath(base_dir), 'symba', 'src', 'plan-ipc')
     call([planner, config, domain, problem, plan])
 
 if __name__ == "__main__":
